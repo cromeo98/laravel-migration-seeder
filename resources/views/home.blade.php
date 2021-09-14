f493a4e1c1bb6c582e4bdedb1a7b87f5a55df97a
cromeo98 <romeo.christian98@gmail.com>
2021-09-14 19:08:36 +0200

    end of exercise

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,32 @@
 @section('title','homepage')
 
 @section('content')
-    
+
+    <div class="container my-2">
+
+        <div class="row">
+
+            @foreach ($trips as $trip)
+
+                <div class="col-4">
+                    <div class="card my-2" style="width: 18rem;">
+                        <div class="card-body" style="height: 375px">
+
+                            <h3 class="card-title">Trip {{$trip->id}}</h3>
+                            <ul>
+                                <li class="pt-1">Destination: {{$trip->destination}}</li>
+                                <li class="pt-1">Transport: {{$trip->transport}}</li> <li class="pt-1">Description: {{$trip->description}}</li>
+                                <li class="pt-1">Price: {{$trip->price}}</li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+
+            @endforeach
+
+        </div>
+
+    </div>
+
 @endsection
